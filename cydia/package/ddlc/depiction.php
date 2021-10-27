f64f276ffb3760a37a43ec3b238fa435cc946f4f
LordDemecrius83 <89468157+LordDemecrius83@users.noreply.github.com>
2021-10-27 16:19:34 +0800

    1234567890

---
+++ cydia/package/ddlc/depiction.php
@@ -0,0 +1,25 @@
+<html class="cytyle-flat"><html>
+	<head>
+		<title>Doki Doki Literature Club!	· Ren's Cydia Repo</title>
+		<link rel="stylesheet" type="text/css" href="//cydia.saurik.com/cytyle/style-814ed94d029255e832932322f7cc9d7fb2de0071.css">
+		<script type="text/javascript" src="//cydia.saurik.com/cytyle/style-3c578a2a65b28de08a903cddcf2ea0feae341e61.js"></script>
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	</head>
+	<body class="pinstripe">
+	<panel>
+				<label>Description</label>
+		<block>
+		    <p>Compatible with iOS 8 to iOS 10.</p>
+			<p>An unofficial port of Doki Doki Literature Club to iOS using reniOS support files.</p>
+		</block>
+		<label>Changelog</label>
+		<fieldset>
+			<div>
+				<p><strong>Version 1.1.0</strong></p>
+				<hr>
+				<p>- Initial Cydia Release</p>
+			</div>
+		</fieldset>
+	</panel>
+	</body>
+</html>
